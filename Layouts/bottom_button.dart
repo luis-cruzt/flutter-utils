fe43e2ff98d21756c28d81f62a42a7ef2a0fdd78
Luis Cruz <55716036+luis-cruzt@users.noreply.github.com>
2022-04-19 16:32:04 -0500

    Update bottom_button.dart

--- Layouts/bottom_button.dart
+++ Layouts/bottom_button.dart
@@ -5,28 +5,30 @@ class Home extends StatelessWidget {
         appBar: AppBar(
           title: Text('AppBar'),
         ),
-        body: LayoutBuilder(builder: (context, constraints) {
-          return SingleChildScrollView(
-              child: ConstrainedBox(
-                  constraints: BoxConstraints(minWidth: constraints.maxWidth, minHeight: constraints.maxHeight),
-                  child: IntrinsicHeight(
-                    child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Text('header'),
-                          Expanded(
-                            child: Container(
-                              color: Colors.green,
-                              child: Text('body'),
-                            ),
-                          ),
-                          Text('footer'),
-                        ]
+        body: LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: constraints.maxWidth, minHeight: constraints.maxHeight),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('header'),
+                  Expanded(
+                    child: Container(
+                      color: Colors.green,
+                      child: Text('body'),
                     ),
-                  )
-              )
-          );
-        })
+                  ),
+                  Text('footer'),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    )
     );
   }
 }
